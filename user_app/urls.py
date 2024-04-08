675c23833b6557199eef2434157a410776e08e51
Laband <lukeabandeen@gmail.com>
2024-04-08 13:22:10 -0400

    added to user urls

--- user_app/urls.py
+++ user_app/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import CreateUserView, LoginView, VerifyUserView
+from .views import CreateUserView, LoginView, VerifyUserView, ProfileView
 
 urlpatterns = [
   path('register/', CreateUserView.as_view(), name='register'),
   path('login/', LoginView.as_view(), name='login'),
   path('token/refresh/', VerifyUserView.as_view(), name='token_refresh'),
+  path('profile/', ProfileView.as_view(), name='profile'),
 ]
