7af1b62282ed0231185fe5e3df3df517a563debc
Laband <lukeabandeen@gmail.com>
2024-04-08 11:59:44 -0400

    started urls and views for game_app

--- games_app/urls.py
+++ games_app/urls.py
@@ -1,1 +1,8 @@
-#placeholder
+from django.urls import path
+from .views import GameList, GamesInterestedList, GamesPlayingList
+
+urlpatterns = [
+        path('games-provided/', GameList.as_view(), name='game-list'),
+        path('games-interested/<int:user_id>/', GamesInterestedList.as_view(), name='games-interested'),
+        path('games-playing/<int:user_id>/', GamesPlayingList.as_view(), name='games-playing'),
+        ]
