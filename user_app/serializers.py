e2d333fe26f22f81c16e99098ecbf7048409c1d7
Jenn Bourke <jennfbourke@gmail.com>
2024-04-08 12:28:51 -0400

    User serializer, profile and match models added

--- user_app/serializers.py
+++ user_app/serializers.py
@@ -1,2 +1,20 @@
+from django.contrib.auth.models import User 
+from rest_framework import serializers
 
-#hold
+#User serializer
+class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)  
+
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'email')
+    
+    def create(self, validated_data):
+      user = User.objects.create_user(
+          username=validated_data['username'],
+          email=validated_data['email'],
+          password=validated_data['password']  
+      )
+      
+      return user
+
--- user_app/urls.py
+++ user_app/urls.py
@@ -1,1 +1,8 @@
-# placeholder
+from django.urls import path
+from .views import CreateUserView, LoginView, VerifyUserView
+
+urlpatterns = [
+  path('register/', CreateUserView.as_view(), name='register'),
+  path('login/', LoginView.as_view(), name='login'),
+  path('token/refresh/', VerifyUserView.as_view(), name='token_refresh'),
+]
