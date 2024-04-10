f0455fd112da229232fff175cfcd4cf4c9f717fa
Laband <lukeabandeen@gmail.com>
2024-04-10 10:19:42 -0400

    added preferences and its routes etc.

--- user_app/urls.py
+++ user_app/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import CreateUserView, LoginView, VerifyUserView, ProfileView, ProfileDetail, MatchList, MatchDetail
+from .views import CreateUserView, LoginView, VerifyUserView, ProfileView, ProfileDetail, MatchList, MatchDetail, PreferenceDetail, OtherPreferenceDetail
 
 urlpatterns = [
         path('register/', CreateUserView.as_view(), name='register'),
@@ -8,5 +8,7 @@ urlpatterns = [
         path('profile/', ProfileView.as_view(), name='users-profile'),
         path('profile/<int:id>/', ProfileDetail.as_view(), name='others-profile'),
         path('matches/', MatchList.as_view(), name='users-matches'),
-        path('matches/<int:id>/', MatchDetail.as_view(), name='match')
+        path('matches/<int:id>/', MatchDetail.as_view(), name='match'),
+        path('preference/', PreferenceDetail.as_view(), name='preference'),
+        path('preference/<int:id>/', OtherPreferenceDetail.as_view(), name='others-preference'),
         ]
