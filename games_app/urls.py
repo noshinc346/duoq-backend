a8a9e765f4c445983f3f9667afd7c7502015159d
Laband <lukeabandeen@gmail.com>
2024-04-12 10:54:19 -0400

    added an endpoint for getting all usergames of a game

--- games_app/urls.py
+++ games_app/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import GameList, UserGameList, GameDetail, UserGameDetail, Home
+from .views import GameList, UserGameList, GameDetail, UserGameDetail, Home, UserGamesList
 
 
 urlpatterns = [
@@ -8,4 +8,5 @@ urlpatterns = [
         path('games/<int:id>/', GameDetail.as_view(), name='game-detail'),
         path('games/usergames/detail/<int:game_id>/', UserGameDetail.as_view(), name='user-game-detail'),
         path('', Home.as_view(), name='home'),
+        path('games/usergames/for/<int:game_id>/', UserGamesList.as_view(), name='usergames-list'),
         ]
